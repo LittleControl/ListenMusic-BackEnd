3cb892a8e56b8d8db3696a3680e2a67a985b840b
Avalon <littlecontrol2019@gmail.com>
2019-12-23 17:24:48 +0800

    基本的启动程序

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => res.send('Hello, World!'))
+app.listen(3000, () => console.log('App is listening on port 3000'))
